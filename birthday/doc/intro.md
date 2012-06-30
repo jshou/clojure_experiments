36b0a698e6c14e4ee704df270d17753e2b5a0ab0
jshou <joshuahou@gmail.com>
2012-06-29 22:55:30 -0700

    lein new birthday

---
+++ birthday/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to birthday
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ birthday/test/birthday/core_test.clj
@@ -0,0 +1,7 @@
+(ns birthday.core-test
+  (:use clojure.test
+        birthday.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
